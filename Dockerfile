6e0da431ffe79b1e99e593fbd846830c14e93a83
chanduv33 <chandu.teja.0557@gmail.com>
2023-01-27 23:13:04 +0530

    jenkins 18

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,5 @@
 FROM openjdk:8-jdk-alpine
 RUN mkdir app
-RUN chmod +x deploy.sh
 COPY target/config-0.0.1-SNAPSHOT.jar config.jar
 EXPOSE 8089
 ENTRYPOINT ["java","-jar","config.jar"]
--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo ***************************
-echo *******deploy**************
-echo ***************************
-
-VERSION=$1
-
-ssh chandrasekharvemugadda@192.168.0.108 "/home/chandrasekharvemugadda/jenkins/publish $VERSION"
